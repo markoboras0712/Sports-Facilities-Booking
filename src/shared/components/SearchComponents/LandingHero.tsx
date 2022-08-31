40ff3e4717547bb7ba5e85e667b44de2ea412bb4
Marko Boras <marko.boras@prototyp.digital>
2022-08-31 13:41:13 +0200

    Hero image responsive

--- src/shared/components/SearchComponents/LandingHero.tsx
+++ src/shared/components/SearchComponents/LandingHero.tsx
@@ -1,54 +1,43 @@
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import * as React from 'react';
 import { useDeviceSizes } from 'shared/hooks';
 
-const styles = {
-  paperContainer: {
-    backgroundImage: `url(https://img.freepik.com/free-vector/gradient-football-field-background_52683-65681.jpg?w=2000)`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    height: '30vh',
-  },
-};
-
 export const LandingHero: React.FC = () => {
   const { smallDeviceSize } = useDeviceSizes();
-
   return (
     <>
-      {!smallDeviceSize && (
-        <Grid
-          item
+      <Grid
+        item
+        sx={{
+          display: 'flex',
+          width: '100%',
+          backgroundColor: '#EFF4F8',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Box
           sx={{
-            display: 'flex',
-            width: '100%',
-            backgroundColor: '#EFF4F8',
-            flexDirection: 'column',
-            alignItems: 'center',
+            pt: !smallDeviceSize ? 15 : 10,
+            pb: 1,
+          }}
+        >
+          {!smallDeviceSize ? (
+            <Typography variant="h2">THE BALL IS IN YOUR COURT</Typography>
+          ) : (
+            <Typography variant="h4">THE BALL IS IN YOUR COURT</Typography>
+          )}
+        </Box>
+        <Box
+          sx={{
+            pb: !smallDeviceSize ? 1 : 10,
             justifyContent: 'center',
           }}
         >
-          <Box
-            sx={{
-              pt: 15,
-              pb: 1,
-            }}
-          >
-            <Typography variant="h2">THE BALL IS IN YOUR COURT</Typography>
-          </Box>
-          <Box
-            sx={{
-              pb: 1,
-              justifyContent: 'center',
-            }}
-          >
-            <Typography variant="h4">
-              Book a sports facility near you!
-            </Typography>
-          </Box>
-        </Grid>
-      )}
-      {smallDeviceSize && <Paper style={styles.paperContainer} />}
+          <Typography variant="h4">Book a sports facility near you!</Typography>
+        </Box>
+      </Grid>
     </>
   );
 };
